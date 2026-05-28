2a8adccfad0ec3496c70a38b67a3b22d073a69d9
Bruno Gamarra <24100022@ue.edu.pe>
2026-05-28 18:25:59 -0500

    Realizando ajustes de Program y appsettings

---
+++ PC12310064422200111/Controllers/ClienteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PC12310064422200111.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+    }
+}
--- PC12310064422200111/Program.cs
+++ PC12310064422200111/Program.cs
@@ -1,20 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PC1.CORE.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<TallerMecanicoDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DevConnection")
+    ));
+
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseAuthorization();
 
 app.MapControllers();
 
